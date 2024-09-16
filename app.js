5bd712e6be02e755b9b604f51cde88cf77a7405c
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:41:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+let pizzas = [' supreme ', 'creamy hot', ' cheese '];
+console.log("\nPizza names:\n\n");
+for (let i = 0; i < pizzas.length; i++) {
+    console.log(pizzas[i]);
+    console.log(" I like " + pizzas[i] + "pizza");
+}
+console.log("\n I really like pizza\n");
